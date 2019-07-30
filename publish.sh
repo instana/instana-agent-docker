c6092d35491249b9d9ba6d349f5045b9ce572f05
Nathan Fisher <nfisher@junctionbox.ca>
2019-07-30 16:07:39 -0300

    Add publish script to build/publish images

---
+++ publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -eu -o pipefail
+
+# Lame for now, but we need a button to push
+# Love Z
+
+# TODO: Consider a base image for use by the dynamic and static builds.
+
+docker build ./dynamic --build-arg FTP_PROXY=${INSTANA_AGENT_KEY} --no-cache -t instana/agent
+docker build ./static --build-arg FTP_PROXY=${INSTANA_AGENT_KEY} --no-cache -t "instana/agent-static"
+
+docker tag instana/agent instana/agent:${INSTANA_AGENT_RELEASE}
+docker tag instana/agent instana/agent:latest
+
+docker tag "instana/agent-static" "instana/agent-static:${INSTANA_AGENT_RELEASE}"
+docker tag "instana/agent-static" "instana/agent-static:latest"
+
+docker tag instana/agent containers.instana.io/instana/release/agent/dynamic:${INSTANA_AGENT_RELEASE}
+docker tag instana/agent containers.instana.io/instana/release/agent/dynamic:latest
+
+docker tag "instana/agent-static" "containers.instana.io/instana/release/agent/static:${INSTANA_AGENT_RELEASE}"
+docker tag "instana/agent-static" "containers.instana.io/instana/release/agent/static:latest"
+
+if [ "$PUBLISH" = "Yes" ] ; then
+    docker push instana/agent:${INSTANA_AGENT_RELEASE}
+    docker push instana/agent:latest
+
+    docker push "instana/agent-static:${INSTANA_AGENT_RELEASE}"
+    docker push "instana/agent-static:latest"
+
+    docker push containers.instana.io/instana/release/agent/dynamic:${INSTANA_AGENT_RELEASE}
+    docker push containers.instana.io/instana/release/agent/dynamic:latest
+
+    docker push "containers.instana.io/instana/release/agent/static:${INSTANA_AGENT_RELEASE}"
+    docker push "containers.instana.io/instana/release/agent/static:latest"
+fi
