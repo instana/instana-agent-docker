5589d9e2803865bd04690b92dd34c83805d5df31
Michele Mancioppi <michele.mancioppi@instana.com>
2020-07-03 12:26:27 +0200

    Support AWS Mode running on Fargate on ECS

--- dynamic/run.sh
+++ dynamic/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ "${INSTANA_KEY}" == "" ]] && [[ "${INSTANA_AGENT_KEY}" == "" ]]; then
   echo "Please add the INSTANA_AGENT_KEY environment variable and provide your Agent Key!"
   exit 1
@@ -104,40 +106,90 @@ cat /root/com.instana.agent.bootstrap.AgentBootstrap.cfg.tmpl | gomplate > \
 cat /root/com.instana.agent.main.config.UpdateManager.cfg.tmpl | gomplate > \
   /opt/instana/agent/etc/instana/com.instana.agent.main.config.UpdateManager.cfg
 
-if [ ! -z "${INSTANA_AGENT_HTTP_LISTEN}" ]; then
+if [ -n "${INSTANA_AGENT_HTTP_LISTEN}" ]; then
   echo -e "\nhttp.listen = ${INSTANA_AGENT_HTTP_LISTEN}" >> /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
 fi
 
-if [ ! -z "${INSTANA_AGENT_MODE}" ]; then
-  if [ "${INSTANA_AGENT_MODE}" = "AWS" ]; then
+if [ "${INSTANA_AGENT_MODE}" = "AWS" ]; then
+  echo "AWS mode configured"
 
-    INSTANA_AWS_REGION_CONFIG=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document --connect-timeout 2 | awk -F\" '/region/ {print $4}')
+  ###
+  # Discover which platform we are running on
+  ###
+  if [ -n "${ECS_CONTAINER_METADATA_URI}" ]; then
+    PLATFORM='ECS'
+  elif curl -s http://169.254.169.254/latest/meta-data/ --connect-timeout 2 --fail; then
+    PLATFORM='EC2'
+  else
+    PLATFORM='UNKNOWN'
+  fi
 
-    if [ $? != 0 ]; then
-      log_error "Error querying AWS metadata."
+  ###
+  # Retrieve region
+  ###
+  if [ -n "${INSTANA_AWS_REGION_CONFIG}" ]; then
+    echo "AWS region configured via environment: ${INSTANA_AWS_REGION_CONFIG}"
+  else
+    case "${PLATFORM}" in
+    'ECS')
+      AWS_AVAILABILITY_ZONE=$(curl -s "${ECS_CONTAINER_METADATA_URI}/task" --connect-timeout 2 --fail | jq -r '.AvailabilityZone')
+
+      if [[ "${AWS_AVAILABILITY_ZONE}" =~ ^([a-z]+-[a-z]+-[0-9])* ]]; then
+        INSTANA_AWS_REGION_CONFIG="${BASH_REMATCH[1]}"
+      else
+        echo "Cannot parse AWS region from the Availability Zone '${AWS_AVAILABILITY_ZONE}' retrieved from the '${ECS_CONTAINER_METADATA_URI}/task' endpoint"
+        exit 1
+      fi
+      ;;
+    'EC2')
+      INSTANA_AWS_REGION_CONFIG=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document --connect-timeout 2 --fail | jq -r '.region')
+      ;;
+    *)
+      if [ -n "${AWS_DEFAULT_REGION}" ]; then
+        echo "Using the default AWS region '${AWS_DEFAULT_REGION}' set in the environment via the 'AWS_DEFAULT_REGION' environment variable."
+      else
+        echo "Platform not recognized: this agent does not seem to run on EC2 or ECS. Set the 'INSTANA_AWS_REGION_CONFIG' environment variable."
+        exit 1
+      fi
+      ;;
+    esac
+
+    if [ -z "${INSTANA_AWS_REGION_CONFIG}" ]; then
+      echo "Could not retrieve the AWS region from the AWS metadata"
       exit 1
     fi
 
+    echo "Discovered AWS region: ${INSTANA_AWS_REGION_CONFIG}"
+
     export INSTANA_AWS_REGION_CONFIG
-
-    ROLES_FOUND=false
-
-    if ! curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/ --connect-timeout 2 | grep 404&> /dev/null; then
-      ROLES_FOUND=true
-    fi
-
-    if [ "$ROLES_FOUND" = "false" ]; then
-      if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-        echo "AWS_ACCESS_KEY_ID and/or AWS_SECRET_ACCESS_KEY not exported, and no IAM instance role detected to allow AWS API access."
-        exit 1
-      fi
-    fi
-
-    echo -e "\nmode = INFRASTRUCTURE" >> /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
-  else
-    echo -e "\nmode = ${INSTANA_AGENT_MODE}" >> /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
   fi
+
+  ###
+  # Look up roles
+  ###
+  readonly EC2_CREDENTIALS_ENDPOINT='http://169.254.169.254/latest/meta-data/iam/security-credentials/'
+  case "${PLATFORM}" in
+  'EC2')
+    if curl -s "${EC2_CREDENTIALS_ENDPOINT}" --connect-timeout 2 --fail; then
+      echo "IAM roles found using EC2's '${EC2_CREDENTIALS_ENDPOINT}' endpoint."
+    fi
+    ;;
+  # 'ECS')  # On ECS, our agent does not know yet how to use IAM roles exposed there, so we need to go the ACCESS_KEY route
+  *)
+    if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+      echo "Neither 'AWS_ACCESS_KEY_ID' nor 'AWS_SECRET_ACCESS_KEY' environment variables are exported, and no IAM instance role is detected to allow AWS API access. Please configure either the 'AWS_ACCESS_KEY_ID' or the 'AWS_SECRET_ACCESS_KEY' environment variables."
+      exit 1
+    fi
+    ;;
+  esac
 fi
+
+# Normalize the Agent mode to those that are actually used by the agent itself, i.e., APM and INFRASTRUCTURE
+if [ "${INSTANA_AGENT_MODE}" != "APM" ]; then
+  INSTANA_AGENT_MODE='INFRASTRUCTURE'
+fi
+
+echo -e "\nmode = ${INSTANA_AGENT_MODE}" >> /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
 
 if [ -d /host/proc ]; then
   export INSTANA_AGENT_PROC_PATH=/host/proc
@@ -151,7 +203,6 @@ if [ -f /root/crashReport.sh ] && [ -z "${INSTANA_DISABLE_CRASH_REPORT}" ]; then
   # Therefore instead modify the script directly so we can properly include the quotes and spaces are escaped correctly.
   FLAGS="-XX:OnError=\"/opt/instana/agent/crashReport.sh %p\" -XX:ErrorFile=/opt/instana/agent/hs_err.log -XX:OnOutOfMemoryError=\"/opt/instana/agent/crashReport.sh %p 'Out of Memory'\""
   sed -i "s|\ \${JAVA_OPTS}\ |\ \${JAVA_OPTS}\ ${FLAGS} |g" /opt/instana/agent/bin/karaf
-
 fi
 
 echo "Starting Instana Agent ..."
