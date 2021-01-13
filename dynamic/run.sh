93ce2627802375b1026c304930e9d4d5a38d897c
Cedric Ziel <cedric.ziel@instana.com>
2021-01-13 11:03:22 +0100

    Debug gomplate build

--- dynamic/run.sh
+++ dynamic/run.sh
@@ -121,7 +121,8 @@ rm -rf /tmp/* /opt/instana/agent/etc/org.ops4j.pax.logging.cfg \
 cp /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg.template /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
 
 cp /root/configuration.yaml /opt/instana/agent/etc/instana
-gomplate < /root/org.ops4j.pax.logging.cfg.tmpl > /opt/instana/agent/etc/org.ops4j.pax.logging.cfg
+cat /root/org.ops4j.pax.logging.cfg.tmpl | /usr/bin/gomplate > /opt/instana/agent/etc/org.ops4j.pax.logging.cfg
+cat /opt/instana/agent/etc/org.ops4j.pax.logging.cfg
 gomplate < /root/com.instana.agent.main.sender.Backend-1.cfg.tmpl > \
   /opt/instana/agent/etc/instana/com.instana.agent.main.sender.Backend-1.cfg
 
