f7c5f54270a04914e0d81f1e478b6dd724b442d7
Nathan Fisher <nfisher@junctionbox.ca>
2021-01-25 14:58:55 -0500

    Add kubernetes agent mode to run scripts

--- dynamic/run.sh
+++ dynamic/run.sh
@@ -234,6 +234,8 @@ case "${INSTANA_AGENT_MODE}" in
   'AWS')
     INSTANA_AGENT_MODE='INFRASTRUCTURE'
     ;; # The AWS agent mode is infra + AWS sensor setup
+  'KUBERNETES')
+    ;;
   'INFRASTRUCTURE')
     ;;
   'APM')
